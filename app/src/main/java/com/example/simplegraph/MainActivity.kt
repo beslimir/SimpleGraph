b3422748c7b1cd0cc2df0d610ae1076a40eeaf94
beslimir <marin.beslo@gmail.com>
2022-06-07 12:32:10 +0200

    remove unnecessary imports

--- app/src/main/java/com/example/simplegraph/MainActivity.kt
+++ app/src/main/java/com/example/simplegraph/MainActivity.kt
@@ -5,16 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.simplegraph.ui.theme.SimpleGraphTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
